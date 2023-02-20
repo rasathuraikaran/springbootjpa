a6bbb89857034d82461b07d54e53aaff68e4c22d
Rasathurai Karan <73582838+rasathuraikaran@users.noreply.github.com>
2023-02-20 22:32:21 +0530

    Alien model createed

---
+++ src/main/java/model/Alien.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Alien {
+    private int aid;
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    private String aname;
+
+    @Override
+    public String toString() {
+        return "Alien{" +
+                "aid=" + aid +
+                ", aname='" + aname + '\'' +
+                '}';
+    }
+}
