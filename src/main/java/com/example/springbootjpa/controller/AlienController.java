a2a99f2db47ab99835a716948f91da1e1becd5d8
Rasathurai Karan <73582838+rasathuraikaran@users.noreply.github.com>
2023-02-20 23:10:41 +0530

    Tomcat jasper depency added+

--- src/main/java/com/example/springbootjpa/controller/AlienController.java
+++ src/main/java/com/example/springbootjpa/controller/AlienController.java
@@ -1,11 +1,11 @@
-package controller;
+package com.example.springbootjpa.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AlienController {
-    @RequestMapping("home")
+    @RequestMapping("/")
     public String home(){
         System.out.println("Hi thus srk");
         return "home.jsp";
